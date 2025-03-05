f3175c243af7bd4f455a564764e31868131266f3
florian-alberge <148914461+florian-alberge@users.noreply.github.com>
2025-03-05 16:05:33 -0700

    Update MilkTooth_text.js

--- MilkTooth_text.js
+++ MilkTooth_text.js
@@ -9,6 +9,124 @@ window.addEventListener("DOMContentLoaded", (event) => {
     function createScrollTrigger(triggerElement, timeline) {
         // Reset tl when scroll out of view past bottom of screen
         ScrollTrigger.create({
+            trigger: triggerElement,
+            start: "top bottom",
+            onLeaveBack: () => {
+                timeline.progress(0);
+                timeline.pause();
+            }
+        });
+        // Play tl when scrolled into view (60% from top of screen)
+        ScrollTrigger.create({
+            trigger: triggerElement,
+            start: "top 60%",
+            onEnter: () => timeline.play()
+        });
+    }
+
+    $("[words-slide-up]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".word"), { opacity: 0, yPercent: 100, duration: 1, ease: "back.out(2)", stagger: { amount: 0.5 } })
+          .to($(this).find(".word"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[words-rotate-in]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.set($(this).find(".word"), { transformPerspective: 1000 });
+        tl.from($(this).find(".word"), { rotationX: -90, duration: 0.6, ease: "power2.out", stagger: { amount: 0.6 } })
+          .to($(this).find(".word"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[words-slide-from-right]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".word"), { opacity: 0, x: "1em", duration: 0.6, ease: "power2.out", stagger: { amount: 0.2 } })
+          .to($(this).find(".word"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[letters-slide-up]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".char"), { yPercent: 100, duration: 0.2, ease: "power1.out", stagger: { amount: 0.6 } })
+          .to($(this).find(".char"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[letters-slide-down]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".char"), { yPercent: -120, duration: 0.3, ease: "power1.out", stagger: { amount: 0.7 } })
+          .to($(this).find(".char"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[letters-fade-in]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".char"), { opacity: 0, duration: 0.2, ease: "power1.out", stagger: { amount: 0.8 } })
+          .to($(this).find(".char"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[letters-fade-in-random]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".char"), { opacity: 0, duration: 0.05, ease: "power1.out", stagger: { amount: 0.4, from: "random" } })
+          .to($(this).find(".char"), { opacity: 0.7, duration: 0 });
+        createScrollTrigger($(this), tl);
+    });
+
+    $("[scrub-each-word]").each(function (index) {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: $(this),
+                start: "top 90%",
+                end: "top center",
+                scrub: true
+            }
+        });
+        tl.from($(this).find(".word"), { opacity: 0, duration: 0.2, ease: "power1.out", stagger: { each: 0.4 } })
+          .to($(this).find(".word"), { opacity: 0.7, duration: 0 });
+    });
+    //Remove this line.
+    // gsap.set("[text-split]", { opacity: 0.7 });
+
+    //Lenis update code.
+    const lenis = new Lenis();
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Split text into spans
+    let typeSplit = new SplitType("[text-split]", {
+        types: "words, chars",
+        tagName: "span"
+    });
+
+    // Link timelines to scroll position
+    function createScrollTrigger(triggerElement, timeline) {
+        // Reset tl when scroll out of view past bottom of screen
+        ScrollTrigger.create({
             trigger: $(this),
             start: "top bottom",
             onLeaveBack: () => {
