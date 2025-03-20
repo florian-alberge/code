40954f95d0fc426c7dd975aad4798a0091ff33bd
florian-alberge <148914461+florian-alberge@users.noreply.github.com>
2025-03-20 15:43:35 -0600

    Create milktooth-video.js

---
+++ milktooth-video.js
@@ -0,0 +1,48 @@
+function throttle(func, delay) {
+    let lastCall = 0;
+    return function (...args) {
+        const now = new Date().getTime();
+        if (now - lastCall < delay) {
+            return;
+        }
+        lastCall = now;
+        return func(...args);
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    let pawnShopVideo = document.querySelector(".pawn-shop_video");
+    let heleneVideo = document.querySelector(".helene_video");
+    let milkshakeVideo = document.querySelector(".milkshake_video"); // Added milkshake_video selector
+
+    function setupScrubbing(videoElement, triggerElement) {
+        videoElement.onloadedmetadata = function() {
+            gsap.to(videoElement, {
+                currentTime: videoElement.duration,
+                ease: "none",
+                scrollTrigger: {
+                    trigger: triggerElement,
+                    start: "top top+=50vh",
+                    end: "bottom bottom",
+                    scrub: true,
+                    markers: false, // Set to true for debugging
+                    onUpdate: (self) =>
+                },
+            });
+        };
+    }
+
+    if (pawnShopVideo) {
+        setupScrubbing(pawnShopVideo, ".h-scrolling-height.tv-pawnshop");
+    }
+
+    if (heleneVideo) {
+        setupScrubbing(heleneVideo, ".h-scrolling-height.tv-helene");
+    }
+
+    if (milkshakeVideo) {
+        setupScrubbing(milkshakeVideo, ".h-scrolling-height.tv-milkshake"); // Added trigger for milkshake
+    }
+});
